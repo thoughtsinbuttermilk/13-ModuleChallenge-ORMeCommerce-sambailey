311c24732af25ea4a12fb83375f4c20d1af2d408
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-09-03 14:06:48 -0500

    created attributes for each column in product table, added validation to check datatypes when model is saved

--- models/Product.js
+++ models/Product.js
@@ -4,13 +4,44 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
+// create the attributes for each column of the table
 Product.init(
   {
     // define columns
-  },
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // validates this column every time the model is saved
+      validate: {
+        isDecimal: true,
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+        validate: {
+          isNumeric: true,
+        },
+        catgoy_id: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: 'category',
+            key: 'id'
+          }
+        },
   {
     sequelize,
     timestamps: false,
