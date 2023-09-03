00458b289ae31eadfe3328b0498543413cb31131
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-09-03 14:43:31 -0500

    fixed unpaired curly braces

--- models/Product.js
+++ models/Product.js
@@ -1,23 +1,24 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 // add category
-const category = require('../models/category');
+const category = require("../models/category");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model { }
+class Product extends Model {}
 
 // set up fields and rules for Product model
-// create the attributes for each column of the table
 Product.init(
   {
     // define columns
+    // create the attributes for each column of the table
+
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     product_name: {
       type: DataTypes.STRING,
@@ -30,26 +31,29 @@ Product.init(
       validate: {
         isDecimal: true,
       },
-      stock: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 10,
-        validate: {
-          isNumeric: true,
-        },
-        catgoy_id: {
-          type: DataTypes.INTEGER,
-          references: {
-            model: 'category',
-            key: 'id'
-          }
-        },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
+  },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
