8613dda08bc5e509cd46a770ca1273f55e41201a
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-09-03 13:52:54 -0500

    defined columns for tags

--- models/Tag.js
+++ models/Tag.js
@@ -1,19 +1,39 @@
-const { Model, DataTypes } = require('sequelize');
+// TODO: from requirements
+// Tag
+// id
+// Integer
+// Doesn't allow null values
+// Set as primary key
+// Uses auto increment
+// tag_name
+// String
 
-const sequelize = require('../config/connection.js');
+
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
