795ac619999762088f0778debf4c681a3cde1dd3
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-09-03 14:23:48 -0500

    correction: added require for category

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,8 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+// add category
+const category =  require('../models/category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model { }
