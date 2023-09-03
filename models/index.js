8ca604756562b148bfd116b1fcf92fa112abfd55
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-09-03 13:54:33 -0500

    created associations for sequelize models (i hope!)

--- models/index.js
+++ models/index.js
@@ -1,3 +1,15 @@
+// TODO: from requirements
+// Product belongs to Category
+// and Category has many Product models
+// as a category can have multiple products
+// but a product can only belong to one category
+
+// Product belongs to many Tag models
+// and Tag belongs to many Product models
+// Allow products to have multiple tags
+// and tags to have many products by
+// using the ProductTag through model
+
 // import models
 const Product = require("./Product");
 const Category = require("./Category");
