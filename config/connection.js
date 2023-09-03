50631efcc5e1455f01202159aba6b35a22039b31
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-09-03 17:07:20 -0500

    changed localhost to 127.0.0.1, added link to JAWS to learn more later

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+// https://www.jawsdb.com/
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+      host: '127.0.0.1',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
