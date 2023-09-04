471f215adcd2921a5a8c465c3ea1f20c5a72d8f2
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-09-04 11:52:36 -0500

    added update (put) and delete routes for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,6 +49,8 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   // create a new category
+  // INVESTIGATE: use findOrCreate?
+  // https://sequelize.org/docs/v6/core-concepts/model-querying-finders/#findorcreate
   Category.create({
     category_name: req.body.category_name,
   })
@@ -61,10 +63,42 @@ router.post("/", (req, res) => {
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((categoryData) => {
+      if (!categoryData) {
+        res.status(404).json({ message: "category not found" });
+        return;
+      }
+      res.json(categoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((categoryData) => {
+      if (!dbCatData) {
+        res.status(404).json({ message: "category not found" });
+        return;
+      }
+      res.json(categoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
