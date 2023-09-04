af5b7eef4484756c17b214fe2883b69d53bcb7c7
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-09-04 16:45:01 -0500

    added comments: http request URL construction

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,10 @@ const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
+// used to construct the endpoint
+// http://127.0.0.1:3001/categories
+// http://127.0.0.1:3001/products
+// http://127.0.0.1:3001/tags
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
